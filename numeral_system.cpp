a3a04ef92adba24d30977bd5b361b79f8a7306ca
EricYangsw <soilwater0723@gmail.com>
2019-03-08 23:04:42 +0800

    simple numeral_system.cpp

--- numeral_system.cpp
+++ numeral_system.cpp
@@ -1,45 +1,41 @@
 #include <iostream>
 #include <stdio.h>
+#include <string>
 using namespace std;
 
-
+/*
+void quick_tobinary(int n){
+    string r;
+    while(n!=0) {r=(n%2==0 ?"0":"1")+r; n/=2;}
+    cout << "quick_tobinary：" << r << endl;
+}
+*/
 
 void numeral_convert(int decimal, const int convert){
-    char binary_result[100];
-
+    string r;
     int i = 0;
+    
     while (decimal != 0)
     {
         int remainder = 0;
         remainder = decimal % convert;
 
         if (remainder<10)
-            binary_result[i] = remainder + '0';
-        else{
-            switch(remainder){
-                case 10: binary_result[i] = 'A';
-                case 11: binary_result[i] = 'B';
-                case 12: binary_result[i] = 'C';
-                case 13: binary_result[i] = 'D';
-                case 14: binary_result[i] = 'E';
-                case 15: binary_result[i] = 'F';
-            }
+            r = to_string(remainder) + r;
+        else
+        {
+            r = (char)(remainder + 45);
         }
         i++;
         decimal = decimal / convert;
     }
-    cout << "ANS: ";
-    for (int idx = i-1; idx >= 0; idx--)
-        cout << binary_result[idx];
+    cout << "ANS: " << r << endl;
 }
-
-
 
 
 int main(){
     int decimal = 0;
     int c = 0;
-
     cout << "input a integer number: ___ (decimal)" << endl;
     cin >> decimal;
 
@@ -48,8 +44,8 @@ int main(){
             << "  -Input  8 : Octal      !" << "\n"
             << "  -Input 16 : Hexadecimal!" << endl;
     cin >> c;
-
     numeral_convert(decimal, c);
-
+    //quick_tobinary(decimal);
     return 0;
 }
+
